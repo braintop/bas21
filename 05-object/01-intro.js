264781f844b28695614d64be3d6bbffe0e98b69d
braintop <braintop7@gmail.com>
2025-11-03 09:54:59 +0200

    add

---
+++ 05-object/01-intro.js
@@ -0,0 +1,35 @@
+let arr = ["oren", 20, "pizza", 40, 50];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr.length);
+console.log(arr[arr.length - 1]);
+
+let person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+console.log(person["name"]);
+console.log(person["age"]);
+console.log(person["city"]);
+let obj = {
+};
+
+obj.name = "Jane";
+obj.age = 20;
+obj.city = "Los Angeles";
+console.log(obj);
+console.log(obj.name);
+console.log(obj.age);
+console.log(obj.city);
+console.log(obj["name"]);
+console.log(obj["age"]);
+console.log(obj["city"]);
---
+++ 05-object/02-scan-object.js
@@ -0,0 +1,18 @@
+let person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+let keys = Object.keys(person);// ["name", "age", "city"]
+for (let i = 0; i < keys.length; i++) {
+    console.log(keys[i]);
+}
+let values = Object.values(person);// [30, "New York", "John"]
+for (let i = 0; i < values.length; i++) {   
+    console.log(values[i]);
+}
+
+for (let key in person) {
+    console.log(key);
+    console.log(person[key]);
+}

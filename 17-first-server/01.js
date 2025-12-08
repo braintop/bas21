7a6bcaf8fe61b4fe9ad14cf261e0b2ca4c2d0709
braintop <braintop7@gmail.com>
2025-12-08 15:36:45 +0200

    add

--- 17-first-server/01.js
+++ 17-first-server/01.js
@@ -1,7 +1,15 @@
 let http = require("http");
 let serverFunction = (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello Worlde2e2e2e");
+    try{
+        console.log(req);
+        console.log("Request received:" + req.url);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("Hello Worlde2e2e2e");
+    }
+    catch(error){
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("Error: " + error);
+    }
 }
 let server = http.createServer(serverFunction)
 server.listen(3000);

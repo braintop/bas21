09343b3d626b081b57f202d2878814df2bc01fc7
braintop <braintop7@gmail.com>
2025-12-15 13:05:15 +0200

    add

--- 18-express/index.js
+++ 18-express/index.js
@@ -1,6 +1,33 @@
 let express = require('express');
 let app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    console.log(req.url);
+    res.send('Hello daniel');
+});
+
+app.get('/about', (req, res) => {
+    console.log(req.url);
+
+    res.send('About page');
+});
+
+app.get('/contact', (req, res) => {
+    console.log(req.url);
+
+    res.send('Contact page');
+});
+
+app.get('/products', (req, res) => {
+    res.send('Products page');
+});
+app.get('/person/dodi', (req, res) => {
+    console.log(req.url);
+
+    res.send('person dodi');
+});
+
+app.get('/yooda', (req, res) => {
+
+    res.send('hi yooda');
 });
 app.listen(3000);

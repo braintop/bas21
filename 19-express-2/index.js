4452562acdeb7a3680a62e4d5212281808236a23
braintop <braintop7@gmail.com>
2025-12-22 10:16:06 +0200

    add

--- 19-express-2/index.js
+++ 19-express-2/index.js
@@ -1,16 +1,17 @@
-let app = require('express')();
-
+let express = require('express');
+let app = express();
+app.use(express.json());
 console.log("hi app");
-let person = [{name: "John", age: 30, city: "New York"}];
+let persons = [{name: "John", age: 30, city: "New York"}];
 app.get('/', (req, res) => {
     console.log("get persons");  
-
-    res.json(person);
+    res.json(persons);
 });
 
 app.post('/', (req, res) => { 
-    console.log("person added");  
-    res.json("added person");
+    console.log(req.body);
+    persons.push(req.body);
+    res.json(persons);
 });
 app.listen(3000, () => {
     console.log('Server is running on port 3000');

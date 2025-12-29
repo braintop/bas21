b0e8e76597b2b0c34a52201291cab38511ef29a0
braintop <braintop7@gmail.com>
2025-12-29 12:32:31 +0200

    add

--- 23-mogno copy/models/Product.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require("mongoose");
-let Schema = mongoose.Schema;
-let ProductSchema = new Schema({
-    title: String,
-    Description: String,
-    price: Number
-});
-module.exports = mongoose.model('product', ProductSchema,"products");
